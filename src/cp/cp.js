764ed414d0e09828e300c04e43195315254c0de5
Kornull <u.karnialiuk@gmail.com>
2025-04-28 12:24:40 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,16 @@
+import { fork } from "node:child_process";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const fileName = "script.js";
+    const folder = "files";
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const pathToFile = join(__dirname, folder, fileName);
+
+    fork(pathToFile, args).on("message", (res) => process.stdout.write(res));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(["argument-1", "argument-2", "argument-3", "argument-4"]);
