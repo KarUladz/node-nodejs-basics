783c02acfa0daca8c86c27ce97f0c6405c37cb4f
Kornull <u.karnialiuk@gmail.com>
2025-04-28 11:21:36 +0300

    feat: add zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,24 @@
+import {createReadStream, createWriteStream} from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+import { dirname, join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { createGzip } from 'node:zlib';
+
+
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const FOLDER = 'files';
+    const FILE = 'fileToCompress.txt';
+    const GZ = 'archive.gz';
+    const PATH_TO_FOLDER = join(__dirname, FOLDER);
+
+    const rs = createReadStream(join(PATH_TO_FOLDER, FILE))
+    const ws = createWriteStream(join(PATH_TO_FOLDER, GZ))
+    await pipeline(rs, createGzip(), ws)
 };
 
 await compress();
