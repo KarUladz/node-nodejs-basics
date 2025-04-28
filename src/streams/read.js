98e5d8d069a51d39dd5c2cec297411797bf40c03
Kornull <u.karnialiuk@gmail.com>
2025-04-28 10:59:17 +0300

    feat: add streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,26 @@
+import { createReadStream } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const FOLDER = "files";
+    const FILE = "fileToRead.txt";
+    const PATH_TO_FILE = join(__dirname, FOLDER, FILE);
+
+    const rs = createReadStream(PATH_TO_FILE, {encoding: "utf-8"});
+
+    rs.on('data',(data) => {
+        process.stdout.write(data);
+    });
+    rs.on("error", (err) => {
+        process.stdout.write("STREAMS operation failed");
+    });
+    rs.on("end", () => {
+        process.stdout.write("\n");
+    });
 };
 
 await read();
